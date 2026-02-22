4d0a55fa709b4b9de993b8541f08045ed63909ff
JitendriBurle <ji28tu@gmail.com>
2026-02-22 16:47:13 +0530

    Added createBankAccount

--- JavaScript/createBankAccount.js
+++ JavaScript/createBankAccount.js
@@ -14,5 +14,33 @@ function createBankAccount(){
         return balance;
     }
 
-    function withdraw
+    function withdraw(amount) {
+        if(amount <= 0){
+            return "Error: The withdrawal amount should be positive";
+        }
+        if(amount > balance){
+            return "Error: Insufficient balance";
+        }
+        balance -= amount;
+        transactionHistory.push({
+            type: "withdraw",
+            amount
+        })
+        return balance;
+    }
+
+    function getBalance(){
+        return balance;
+    }
+
+    function getTransactionHistory() {
+        return [...transactionHistory];
+    }
+
+    return {
+        deposit,
+        withdraw,
+        getBalance,
+        getTransactionHistory
+    };
 }
